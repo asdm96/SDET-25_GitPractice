66d6701fd96ec4bdf3289578679834fb358741ef
LENOVO <LENOVO@LAPTOP-JOF7DOEN>
2021-12-09 19:15:51 +0530

    Commit 2 User 1 added syso

--- src/test/java/DemoPractice/DemoTest1.java
+++ src/test/java/DemoPractice/DemoTest1.java
@@ -6,6 +6,9 @@ public class DemoTest1 {
 	@Test
 	public void Test1() {
 		System.out.println("User1_Test1");
+		System.out.println("User1_Test1_Made Changes");
+		System.out.println("User1_Test2_Made Changes");
+		System.out.println("User1_Test3_Made Changes");
 	}
 
 }
